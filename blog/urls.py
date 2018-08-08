3a4787a82968f1485a1f8dc8cba7e41cd09376e2
Rafeh Qazi <qazi@cleverprogrammer.com>
2018-08-07 19:34:43 -0700

    Add feature to edit blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     # 127.0.0.1:8000/post/new --> local
     # mydjangosite.com/post/new --> online
     path('post/new/', views.post_new, name='post_new'),
+
+    # 127.0.0.1:8000/post/2/edit --> local
+    # mydjangosite.com/post/2/edit --> online
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -31,3 +31,21 @@ def post_new(request):
         form = PostForm()
         stuff_for_frontend = {'form': form}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+
+        # updating an existing form
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        stuff_for_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_for_frontend)
